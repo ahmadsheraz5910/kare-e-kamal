2eb99ac2425b876fc5ab01bf9e4004c15ce15a16
devnerd591 <devnerd591@gmail.com>
2022-09-04 15:33:16 +0500

    Mobile Responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,10 +26,10 @@ const Home: NextPage = () => {
   return (
     <div className = "m-auto max-w-lg">
       <header className = "p-4 text-center">
-        <h1 className ="text-6xl font-semibold">{Content.index.title}</h1>
+        <h1 className ="text-4xl md:text-6xl font-semibold">{Content.index.title}</h1>
       </header>
       <section className = "p-4 my-4">
-        <h4 className ="text-2xl font-semibold">{Content.index.section.title}</h4>
+        {/* <h4 className ="text-2xl font-semibold">{Content.index.section.title}</h4> */}
         {Content.index.section.payment_options.map((option, idx) => 
             <Tooltip 
               componentsProps={{
@@ -59,15 +59,15 @@ const Home: NextPage = () => {
                 className = "max-w-md my-4 p-4 border rounded-md shadow-md bg-slate-50 text-gray-700 cursor-pointer hover:bg-slate-100"
                 onClick = {() => copy_to_clipboard(value_to_copy.current)}
               >
-                <div style = {{gridTemplateColumns:"0.4fr 0.6fr"}} className = "grid grid-flow-col">
+                <div className = "grid grid-flow-row grid-rows-1 md:grid-cols-[0.4fr,0.6fr] md:grid-flow-col">
                   <p className=  "font-medium">{"Account Number"}</p>
                   <p>{option.account_number}</p>
                 </div>
-                <div style = {{gridTemplateColumns:"0.4fr 0.6fr"}} className = "grid grid-flow-col">
+                <div className = "grid grid-flow-row grid-rows-1 md:grid-cols-[0.4fr,0.6fr] md:grid-flow-col">
                   <p className=  "font-medium">{"Bank Name"}</p>
                   <p>{option.bank_name}</p>
                 </div>
-                <div style = {{gridTemplateColumns:"0.4fr 0.6fr"}} className = "grid grid-flow-col">
+                <div className = "grid grid-flow-row grid-rows-1 md:grid-cols-[0.4fr,0.6fr] md:grid-flow-col">
                   <p className=  "font-medium">{"Account Title"}</p>
                   <p>{option.account_title}</p>
                 </div>
